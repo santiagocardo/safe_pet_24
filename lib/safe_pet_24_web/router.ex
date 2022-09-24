c05c9e5ca5612819f13829e4336a78c6bc3c98b2
Santiago Cardona <santiagocardo80@gmail.com>
2022-09-24 18:56:40 -0500

    Fix confirm email route

--- lib/safe_pet_24_web/router.ex
+++ lib/safe_pet_24_web/router.ex
@@ -83,7 +83,6 @@ defmodule SafePet24Web.Router do
 
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
-    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
   end
 
   scope "/", SafePet24Web do
@@ -96,5 +95,6 @@ defmodule SafePet24Web.Router do
     post "/users/confirm", UserConfirmationController, :create
     get "/users/confirm/:token", UserConfirmationController, :edit
     post "/users/confirm/:token", UserConfirmationController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
   end
 end
